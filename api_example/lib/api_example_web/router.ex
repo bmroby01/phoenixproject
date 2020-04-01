32c7d47c397c40d31249170b5d8b09ed370f06d7
bmroby01 <brianna124@gmail.com>
2020-04-01 14:03:05 -0400

    Added user_controller

--- api_example/lib/api_example_web/router.ex
+++ api_example/lib/api_example_web/router.ex
@@ -2,25 +2,27 @@ defmodule ApiExampleWeb.Router do
   use ApiExampleWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
-  scope "/", ApiExampleWeb do
-    pipe_through :browser
+  # scope "/", ApiExampleWeb do
+  #   pipe_through :browser
 
-    get "/", PageController, :index
-  end
+  #   get "/", PageController, :index
+  # end
 
   # Other scopes may use custom stacks.
-  # scope "/api", ApiExampleWeb do
-  #   pipe_through :api
-  # end
+  scope "/api/v1", ApiExampleWeb do
+    pipe_through(:api)
+
+    get("/users", UserController, :index)
+  end
 end
